785efab7d320d98efb6f7dfb3b1c65c76cfd165b
Erik Nelson <io@eriknelson.me>
2016-01-24 15:36:39 -0500

    Remove about string from config.example

--- test/config.example.js
+++ test/config.example.js
@@ -2,15 +2,13 @@
 
 const NAME = 'flatlineEX'
 const VERSION = `${NAME}.0.1.0+vbomb`
-const ABOUT__INFO_STRING = 'XXX'
 
 const config = {
   channels: ['#flatline_test'],
   server: 'irc.quakenet.org',
   botName: NAME,
   userName: NAME,
-  version: VERSION,
-
+  version: VERSION
 };
 
 module.exports = config;
